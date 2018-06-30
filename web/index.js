7b7ac37f41a9be03c73e0f28bcfb918e5ac7299c
jamescm <james.cory.m@gmail.com>
2018-06-30 00:11:03 -0400

    Redid example for new tutorial

---
+++ web/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const Sequelize = require('sequelize')
+const { MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env
+console.log(process.env)
+const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {
+  host: 'database', // We're using the container name as a host name
+  dialect: 'mysql'
+})
+
+const app = express()
+app.get('/', async (req, res) => {
+  try {
+    await sequelize.authenticate()
+    res.send('Hello from MySQL!')
+  } catch (err) {
+    res.send(err)
+  }
+})
+app.listen(3000, () => console.log('Express listening on port 3000'))
